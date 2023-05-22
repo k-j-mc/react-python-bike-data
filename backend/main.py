c92c7af49731998298b22810fa1a3b21da7215e4
k-j-mc <kristofermcc@gmail.com>
2023-05-22 15:10:36 +0300

    docker files and mongo client

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,30 @@
-from flask import Flask
-from flask_restx import Api
+import os
+from flask import Flask, request, jsonify
+import requests
+from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import mongo_client
 
-app = Flask(__name__, static_url_path="/")
+journeys = mongo_client.journeys
+journey_collection = journeys.journey
+
+load_dotenv(dotenv_path="./.env.local")
+
+FIRST_JOUREY_URL = "https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv"
+SECOND_JOUREY_URL = "https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv"
+THIRD_JOUREY_URL = "https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv"
+
+STATION_URL = (
+    "https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv"
+)
+
+DEBUG = bool(os.environ.get("DEBUG", True))
+
+app = Flask(__name__)
 
 CORS(app)
 
-api = Api(app, doc="/")
+app.config["DEBUG"] = DEBUG
 
 
 if __name__ == "__main__":
